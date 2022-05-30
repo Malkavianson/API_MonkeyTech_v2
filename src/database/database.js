1a30992057e425915f804eaaf05b9c5fc61e369a
Malkavianson <alexneto.best@hotmail.com>
2022-05-30 13:42:20 -0300

    return database

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { connect } = mongoose;
 
 export const conectarAoDatabase = () => {
-  connect(/*process.env.MONGODB_URI || 'mongodb+srv://malkavianson:sad@produtos.ihvk6cf.mongodb.net/?retryWrites=true&w=majority' ||*/ 'mongodb://localhost:27017/produtos', {
+  connect(process.env.MONGODB_URI || 'mongodb+srv://malkavianson:sad@produtos.ihvk6cf.mongodb.net/?retryWrites=true&w=majority'/* || 'mongodb://localhost:27017/produtos'*/, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
